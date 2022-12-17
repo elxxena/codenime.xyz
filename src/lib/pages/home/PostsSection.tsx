dc4848a03489f54b88e8307d72402fe398d33607
4ndrexyz <andrekusuma388@gmail.com>
2022-12-17 23:36:58 +0700

    main

--- src/lib/pages/home/PostsSection.tsx
+++ src/lib/pages/home/PostsSection.tsx
@@ -1,51 +1,67 @@
-import { Box, Button, Grid, Heading, Stack } from "@chakra-ui/react";
-import type { Blog } from "contentlayer/generated";
-import Link from "next/link";
-import { FaArrowRight } from "react-icons/fa";
+import { Heading, Box, Text, Grid } from "@chakra-ui/react";
+import debounce from "lodash-es/debounce";
+import { NextSeo } from "next-seo";
+import dynamic from "next/dynamic";
+import * as React from "react";
 
-import BlogPostPreview from "lib/components/blog/BlogPostPreview";
-import { EVENT_TYPE_NAVIGATE } from "lib/constants/tracking";
-import { trackEvent } from "lib/utils/trackEvent";
+import BlogPostCard from "lib/components/blog/BlogPostCard";
+import MotionGrid from "lib/components/motion/MotionGrid";
+import {
+  childAnimationProps,
+  staggerAnimationProps,
+} from "lib/constants/animation";
 
-export type PostsSectionProps = {
-  data: Array<Blog>;
-};
+import type { BlogPostListProps } from "lib/pages/blog/list/types";
 
-const PostsSection = ({ data }: PostsSectionProps) => {
-  const handleClickViewAllPosts = () => {
-    trackEvent({
-      eventName: "Home: View All Posts",
-      eventData: { type: EVENT_TYPE_NAVIGATE },
-    });
-  };
+const BlogPostSearch = dynamic(
+  () => import("lib/components/blog/BlogPostSearch"),
+  {
+    ssr: false,
+  }
+);
+
+const PostsSection = ({ allPostsData }: BlogPostListProps) => {
+  const [keyword, setKeyword] = React.useState<string>("");
+
+  const filteredPosts = allPostsData.filter((post) =>
+    post.title.toLowerCase().includes(keyword.toLowerCase())
+  );
+
+  const handleChangeKeyword = React.useCallback(
+    () =>
+      debounce((event: React.ChangeEvent<HTMLInputElement>) => {
+        setKeyword(event.target.value);
+      }, 150),
+    []
+  );
 
   return (
-    <Stack as="section" spacing={1}>
+    <Box>
+
       <Heading size="lg" marginBottom={2}>
         Recent Posts
       </Heading>
 
-      <Grid gap={8}>
-        {data.map((postData) => (
-          <BlogPostPreview postData={postData} key={postData.id} />
+      <MotionGrid
+        gap={16}
+        marginY={12}
+        gridTemplateColumns={{ md: "repeat(2, 1fr)" }}
+        {...staggerAnimationProps}
+      >
+        {!filteredPosts.length && <Text>No posts found.</Text>}
+        {filteredPosts.map((postData) => (
+          <BlogPostCard
+            wrapperProps={childAnimationProps}
+            postData={postData}
+            key={postData.title}
+          />
         ))}
-      </Grid>
-
-      <Box>
-        <Button
-          as={Link}
-          href="/blog"
-          rightIcon={<FaArrowRight />}
-          paddingX={0}
-          variant="ghost"
-          fontFamily="heading"
-          onClick={handleClickViewAllPosts}
-        >
-          view all posts
-        </Button>
-      </Box>
-    </Stack>
+      </MotionGrid>
+      
+    </Box>
   );
 };
 
 export default PostsSection;
+
+
