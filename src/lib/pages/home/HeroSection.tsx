bed1becd43caa94ecbbe43e7c2773a4f0ce57bbb
4ndrexyz <andrekusuma388@gmail.com>
2022-12-17 22:35:23 +0700

    update

--- src/lib/pages/home/HeroSection.tsx
+++ src/lib/pages/home/HeroSection.tsx
@@ -5,9 +5,9 @@ import { FaGithub } from 'react-icons/fa';
 
 const HeroSection = () => {
   return (
-    <Container height="90vh" alignItems="center" display="grid">
-      <Stack direction="column" spacing={6} alignItems="center">
-        <Box py={2} px={3} bg="teal" w="max-content" color="white" rounded="md" fontSize="sm">
+    <Container p={{ base: 8, sm: 14 }}>
+      <Stack height="90vh" alignItems="center" display="grid">
+        <Box py={2} px={3} bg="teal" w={["100%", "80%", "60%"]} color="white" rounded="md" fontSize="sm">
           <Stack direction={{ base: 'column', sm: 'row' }}>
             <Text fontWeight="bold">Ready, Set, Build! 🚀</Text>
             <Text>Join the Hackathon!</Text>
@@ -17,17 +17,17 @@ const HeroSection = () => {
           fontSize={{ base: '4xl', sm: '5xl' }}
           fontWeight="bold"
           textAlign="center"
-          maxW="600px"
+          maxW={["100%", "80%", "60%"]}
         >
-          Create accessible React apps
+          Create accessible React apps with speed!
         </chakra.h1>
-        <Text maxW="550px" fontSize="xl" textAlign="center" color="gray.500">
+        <Text fontSize={{ base: "md", md: "xl" }} color="description">
           Chakra UI is a simple, modular and accessible component library that gives you the
           building blocks you need to build your React applications.
         </Text>
         <Stack
           direction={{ base: 'column', sm: 'row' }}
-          w={{ base: '100%', sm: 'auto' }}
+          w={["100%", "80%", "60%"]}
           spacing={5}
         >
           <Button
