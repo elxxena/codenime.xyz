e31b6ba7ef703d0a499e54c8e50ef1d803ff428f
4ndrexyz <andrekusuma388@gmail.com>
2022-12-17 22:27:08 +0700

    update

--- src/lib/pages/home/HeroSection.tsx
+++ src/lib/pages/home/HeroSection.tsx
@@ -1,22 +1,59 @@
-import { Box, Heading, Text } from "@chakra-ui/react";
+import * as React from 'react';
+import { Container, chakra, Stack, Text, Button, Box } from '@chakra-ui/react';
+// Here we have used react-icons package for the icons
+import { FaGithub } from 'react-icons/fa';
 
 const HeroSection = () => {
   return (
-    <Box height="90vh" alignItems="center" display="grid">
-      <Box
-        verticalAlign="center"
-        width={["100%", "80%", "60%"]}
-        marginTop={[8, 0]}
-        alignSelf={["none", "center"]}
-      >
-        <Heading as="h1" size="xl" paddingBottom={11}>
-          こんにちは 👋
-        </Heading>
+    <Container p={{ base: 8, sm: 14 }}>
+      <Stack height="90vh" alignItems="center" display="grid">
+        <Box py={2} px={3} bg="teal" w="max-content" color="white" rounded="md" fontSize="sm">
+          <Stack direction={{ base: 'column', sm: 'row' }}>
+            <Text fontWeight="bold">Ready, Set, Build! 🚀</Text>
+            <Text>Join the Hackathon!</Text>
+          </Stack>
+        </Box>
+        <chakra.h1
+          fontSize={{ base: '4xl', sm: '5xl' }}
+          fontWeight="bold"
+          textAlign="center"
+          maxW="600px"
+        >
+          Create accessible React apps with speed!
+        </chakra.h1>
         <Text fontSize={{ base: "md", md: "xl" }} color="description">
-          Selamat datang di website saya, Semoga harimu menyenangkan!
+          Chakra UI is a simple, modular and accessible component library that gives you the
+          building blocks you need to build your React applications.
         </Text>
-      </Box>
-    </Box>
+        <Stack
+          direction={{ base: 'column', sm: 'row' }}
+          w={{ base: '100%', sm: 'auto' }}
+          spacing={5}
+        >
+          <Button
+            colorScheme="teal"
+            variant="outline"
+            rounded="md"
+            size="lg"
+            height="3.5rem"
+            fontSize="1.2rem"
+          >
+            Get Started
+          </Button>
+          <Button
+            leftIcon={<FaGithub />}
+            colorScheme="gray"
+            variant="outline"
+            rounded="md"
+            size="lg"
+            height="3.5rem"
+            fontSize="1.2rem"
+          >
+            Github
+          </Button>
+        </Stack>
+      </Stack>
+    </Container>
   );
 };
 
