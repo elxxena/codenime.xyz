2c8f4c006afd037103d71545a2885733c0a6c7c5
4ndrexyz <andrekusuma388@gmail.com>
2022-12-17 22:25:23 +0700

    update

--- src/lib/pages/home/HeroSection.tsx
+++ src/lib/pages/home/HeroSection.tsx
@@ -1,62 +1,22 @@
-import * as React from 'react';
-import { Container, chakra, Stack, Text, Button, Box } from '@chakra-ui/react';
-// Here we have used react-icons package for the icons
-import { FaGithub } from 'react-icons/fa';
+import { Box, Heading, Text } from "@chakra-ui/react";
 
 const HeroSection = () => {
   return (
-    <Container p={{ base: 8, sm: 14 }}>
-      <Stack direction="column" spacing={6} alignItems="center">
-        <Box py={2} px={3} bg="teal" w="max-content" color="white" rounded="md" fontSize="sm">
-          <Stack direction={{ base: 'column', sm: 'row' }}>
-            <Text fontWeight="bold">Ready, Set, Build! 🚀</Text>
-            <Text>Join the Hackathon!</Text>
-          </Stack>
-        </Box>
-        <chakra.h1
-          fontSize={{ base: '4xl', sm: '5xl' }}
-          fontWeight="bold"
-          textAlign="center"
-          maxW="600px"
-        >
-          Create accessible React apps{' '}
-          <chakra.span color="teal" bg="linear-gradient(transparent 50%, #83e9e7 50%)">
-            with speed
-          </chakra.span>
-        </chakra.h1>
-        <Text maxW="550px" fontSize="xl" textAlign="center" color="gray.500">
-          Chakra UI is a simple, modular and accessible component library that gives you the
-          building blocks you need to build your React applications.
+    <Box height="90vh" alignItems="center" display="grid">
+      <Box
+        verticalAlign="center"
+        width={["100%", "80%", "60%"]}
+        marginTop={[8, 0]}
+        alignSelf={["none", "center"]}
+      >
+        <Heading as="h1" size="xl" paddingBottom={11}>
+          こんにちは 👋
+        </Heading>
+        <Text fontSize={{ base: "md", md: "xl" }} color="description">
+          Selamat datang di website saya, Semoga harimu menyenangkan!
         </Text>
-        <Stack
-          direction={{ base: 'column', sm: 'row' }}
-          w={{ base: '100%', sm: 'auto' }}
-          spacing={5}
-        >
-          <Button
-            colorScheme="teal"
-            variant="outline"
-            rounded="md"
-            size="lg"
-            height="3.5rem"
-            fontSize="1.2rem"
-          >
-            Get Started
-          </Button>
-          <Button
-            leftIcon={<FaGithub />}
-            colorScheme="gray"
-            variant="outline"
-            rounded="md"
-            size="lg"
-            height="3.5rem"
-            fontSize="1.2rem"
-          >
-            Github
-          </Button>
-        </Stack>
-      </Stack>
-    </Container>
+      </Box>
+    </Box>
   );
 };
 
