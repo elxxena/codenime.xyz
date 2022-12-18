cd366b0f88e5e3a4554a86736bdff28eee47dfe0
4ndrexyz <andrekusuma388@gmail.com>
2022-12-19 00:54:59 +0700

    update

--- src/lib/pages/privacy/index.tsx
+++ src/lib/pages/privacy/index.tsx
@@ -25,16 +25,16 @@ const PrivacyPolicy = () => {
         the privacy of our visitors. This Privacy Policy document contains types of 
         information that is collected and recorded by codenime and how we use it
         </Text>
-
+        <br />
         <Text as="p">
         If you have additional questions or require more information about our Privacy Policy, 
         do not hesitate to contact us.
         </Text>
-
-        <Heading as="h3">
+        <br />
+        <Heading as="h4">
           Log Files
         </Heading>
-
+        <br />
         <Text as="p">
         codenime follows a standard procedure of using log files. 
         These files log visitors when they visit websites. 
@@ -46,16 +46,16 @@ const PrivacyPolicy = () => {
         administering the site, tracking users' movement on the website, and gathering demographic 
         information. Our Privacy Policy was created with the help of the Privacy Policy Generator.
         </Text>
-
-        <Heading as="h3">
+        <br />
+        <Heading as="h4">
           Privacy Policies
         </Heading>
-
+        <br />
         <Text as="p">
         You may consult this list to find the Privacy Policy for each of the advertising 
         partners of codenime.
         </Text>
-
+        <br />
         <Text as="p">Third-party ad servers or ad networks uses technologies like cookies, 
         JavaScript, or Web Beacons that are used in their respective advertisements and links 
         that appear on codenime, which are sent directly to users' browser. 
@@ -64,39 +64,38 @@ const PrivacyPolicy = () => {
         campaigns and/or to personalize the advertising content that you see on websites 
         that you visit.
         </Text>
-
-
+        <br />
         <Text as="p">
         Note that codenime has no access to or control over these cookies that are used by third-party advertisers.
         </Text>
-
-        <Heading as="h3">
+        <br />
+        <Heading as="h4">
           Third Party Privacy Policies
         </Heading>
-
+        <br />
         <Text as="p">
         codenime's Privacy Policy does not apply to other advertisers or websites. 
         Thus, we are advising you to consult the respective Privacy Policies of these 
         third-party ad servers for more detailed information. It may include their practices 
         and instructions about how to opt-out of certain options.
         </Text>
-
+        <br />
         <Text as="p">
         You can choose to disable cookies through your individual browser options. 
         To know more detailed information about cookie management with specific web browsers, 
         it can be found at the browsers' respective websites. What Are Cookies?
         </Text>
-
-        <Heading as="h3">
+        <br />
+        <Heading as="h4">
           Children's Information
         </Heading>
-
+        <br />
         <Text as="p">
         Another part of our priority is adding protection for children while using the internet. 
         We encourage parents and guardians to observe, participate in, and/or monitor and guide 
         their online activity.
         </Text>
-
+        <br />
         <Text as="p">
         codenime does not knowingly collect any Personal Identifiable Information from 
         children under the age of 13. If you think that your child provided this kind 
