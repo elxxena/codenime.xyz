0252aa8c85b09203fa7e83ea71859ff1f0428300
4ndrexyz <andrekusuma388@gmail.com>
2022-12-19 13:05:40 +0700

    Spotify

--- src/lib/services/spotify/constants.ts
+++ src/lib/services/spotify/constants.ts
@@ -1,3 +1,3 @@
-export const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID ?? "";
-export const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET ?? "";
-export const SPOTIFY_REFRESH_TOKEN = process.env.SPOTIFY_REFRESH_TOKEN ?? "";
+export const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID ?? "a62c11faae674693adc6aeadd0b8f6ac";
+export const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET ?? "76885e3ff3ce41c2af3bc60ef58cd9bb";
+export const SPOTIFY_REFRESH_TOKEN = process.env.SPOTIFY_REFRESH_TOKEN ?? "AQD5cXRFbJwMnp5Wx12bf8txvDqwkRSd1GmC6eJMlkkf9-mO7UPL8Ihjz2kkVu1Ea01pCN4AlvNw3LjdxV_pPtO8Yace1HIH76OpZ-OXOrH-_yV32GqcbTBNcP5TgtTn36g";
