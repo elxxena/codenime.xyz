e506da7bbeb30ee04b98e5b791de8775ec6f23e6
4ndrexyz <andrekusuma388@gmail.com>
2022-12-19 12:33:57 +0700

    UPDATE

--- src/lib/pages/about/HeadSection.tsx
+++ src/lib/pages/about/HeadSection.tsx
@@ -1,8 +1,15 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, Image } from "@chakra-ui/react";
 
 const HeadSection = () => {
   return (
     <Box marginBottom={16}>
+      <Image
+          mb={5}
+          borderRadius='full'
+          boxSize='150px'
+          src='https://avatars.githubusercontent.com/4ndrexyz'
+          alt='4ndrexyz'
+        />
       <Heading size="2xl" fontWeight="medium">
         4ndrexyz
       </Heading>
