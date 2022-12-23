083d712a758486cff3b455bcd417cfc9c545ce84
4ndrexyz <andrekusuma388@gmail.com>
2022-12-23 22:15:50 +0700

    Update

--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -2,13 +2,15 @@ import { Box } from "@chakra-ui/react";
 
 import HeroSection from "./HeroSection";
 import PostsSection from "./PostsSection";
+import SnippetSection from "./SnippetSection";
 import type { HomeProps } from "./types";
 
-const Home = ({ recentPosts }: HomeProps) => {
+const Home = ({ recentPosts, recentSnippets }: HomeProps) => {
   return (
     <Box marginTop={-24}>
       <HeroSection />
       <PostsSection allPostsData={recentPosts} />
+      <SnippetSection snippets={recentSnippets} />
     </Box>
   );
 };
