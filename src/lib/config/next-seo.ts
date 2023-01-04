66ec04d349caa060fa981d8069577428e0a28848
Andre Firmansah <63025746+4ndrexyz@users.noreply.github.com>
2023-01-04 21:28:10 +0700

    Update next-seo.ts

--- src/lib/config/next-seo.ts
+++ src/lib/config/next-seo.ts
@@ -3,14 +3,14 @@ import type { DefaultSeoProps } from "next-seo";
 // https://github.com/garmeeh/next-seo#nextseo-options
 export const defaultSEOConfig: DefaultSeoProps = {
   title: "codenime",
-  titleTemplate: "%s | codenime",
+  titleTemplate: "%s",
   defaultTitle: "codenime",
-  description: "Tempat yang cocok bagi kamu yang ingin belajar bahasa pemrograman",
+  description: "Tempat yang cocok bagi kamu yang ingin belajar bahasa pemrograman dengan pembahasan yang santai dan mudah dimengerti",
   canonical: "https://codenime.xyz",
   openGraph: {
     url: "https://codenime.xyz",
     title: "codenime",
-    description: "Tempat yang cocok bagi kamu yang ingin belajar bahasa pemrograman",
+    description: "Tempat yang cocok bagi kamu yang ingin belajar bahasa pemrograman dengan pembahasan yang santai dan mudah dimengerti",
     images: [
       {
         url: "https://og.sznm.dev/api/generate?heading=Andre%20Firmansah&text=codenime.xyz",
